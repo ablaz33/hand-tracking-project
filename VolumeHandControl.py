c270450b3ab130307b8d934b8ac7bf24615d0e94
JehezkielJL <jehezkiellatupeirissacode@gmail.com>
2026-07-01 10:53:10 +0700

    final-code

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,8 +1,9 @@
+#pycaw, mediapipe, numpy, openCV
 import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
-
+import math
 ################
 wCam, hCam = 640, 480 
 ################
@@ -12,13 +13,50 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0;
 
-detector = htm.handDetector()
+detector = htm.handDetector(detectionCon = 0.7)
 
+from pycaw.pycaw import AudioUtilities
+device = AudioUtilities.GetSpeakers()
+volume = device.EndpointVolume
+# print(f"Audio output: {device.FriendlyName}")
+# print(f"- Muted: {bool(volume.GetMute())}")
+# print(f"- Volume level: {volume.GetMasterVolumeLevel()} dB")
+# print(f"- Volume range: {volume.GetVolumeRange()[0]} dB - {volume.GetVolumeRange()[1]} dB")
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
 
+    lmList = detector.findPosition(img, draw = False)
+    if len(lmList) != 0:
+        # print(lmList[4], lmList[8])
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2)//2, (y1 + y2)//2
+
+        cv2.circle(img, (x1, y1), 15, (255,0,255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255,0,255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255,0,255), cv2.FILLED)
+
+        length = math.hypot(x2-x1, y2-y1)
+        # print(length)
+
+        vol = np.interp(length, [20, 220], [minVol, maxVol])
+        volBar = np.interp(length, [20, 220], [400, 150])
+
+        print(vol)
+        volume.SetMasterVolumeLevel(vol, None)
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0,255,0), cv2.FILLED)
     
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
